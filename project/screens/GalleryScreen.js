1d243c1995d0c1fd8205293e564f193fdef82bfa
KostArt <ipz204_kav@student.ztu.edu.ua>
2024-03-16 14:48:39 +0200

    Updated GalleryScreen to receive dynamic data from the server with photo uploading

--- project/screens/GalleryScreen.js
+++ project/screens/GalleryScreen.js
@@ -1,26 +1,58 @@
-import { FlatList, StyleSheet, Text, View } from "react-native";
+import { useCallback, useEffect, useState } from "react";
+import { FlatList, Image, StyleSheet, Text, View } from "react-native";
 
 const GalleryItem = ({ item }) => {
+  return <Image style={styles.galleryItem} source={{ uri: item }} />;
+};
+const FakeGalleryItem = ({ item }) => {
   return (
     <View style={styles.galleryItem}>
-      <Text>{item}</Text>
+      <Text></Text>
     </View>
   );
 };
 
-const GalleryScreen = () => {
-  const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+const GalleryScreen = ({ navigation }) => {
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState([
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 1,
+  ]);
+
+  const getGallery = useCallback(async () => {
+    try {
+      setLoading(true);
+      const response = await fetch(
+        "https://raw.githubusercontent.com/ArtemKostuchenko/MobileLabs/main/data/gallery.json"
+      );
+      if (response.status === 200) {
+        const data = await response.json();
+        setData(data.data);
+        setLoading(false);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
+
+  useEffect(() => {
+    const unsubscribe = navigation.addListener("focus", () => {
+      getGallery();
+    });
+
+    return unsubscribe;
+  }, [navigation, getGallery]);
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Фотогалерея</Text>
       <FlatList
         data={data}
-        renderItem={GalleryItem}
+        renderItem={loading ? FakeGalleryItem : GalleryItem}
         keyExtractor={(item) => item.toString()}
         numColumns={2}
         columnWrapperStyle={styles.row}
         contentContainerStyle={styles.gallery}
+        // removeClippedSubviews={false}
       />
     </View>
   );
